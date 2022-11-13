49a27282f00a6dcf9dac2a5a457cafa48dcb316a
Anuppaul <anupkumarpaul20@gmail.com>
2022-11-13 12:59:14 +0530

    finally fixed

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors, unused_local_variable
+
+import 'package:flutter/material.dart';
+import 'screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget buidWidget = Container();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
